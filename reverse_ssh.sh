b406ac3eab3133f6d070ca1293e0945c360eadf0
root <root@core.(none)>
2012-12-14 01:21:34 +0000

    Chnages to accomodate automatic passphrase authentication

--- reverse_ssh.sh
+++ reverse_ssh.sh
@@ -10,7 +10,9 @@
 #ps ax |grep "$COMMAND" |grep -v "grep" > /dev/null 2>&1 || $COMMAND > /dev/null 2>&1
 
 createTunnel() {
-    ssh -i $1 -N -f -R port.kenyaemr.org:$2:localhost:22 -L19922:port.kenyaemr.org:22 childcount@port.kenyaemr.org
+    export SSH_AUTH_SOCK=$1
+    echo $SSH_AUTH_SOCK
+    ssh -v -N -f -R port.kenyaemr.org:$2:localhost:22 -L$3:port.kenyaemr.org:22 root@port.kenyaemr.org
     if [[ $? -eq 0 ]]; then
         echo SSH tunnel created successfully
     else
@@ -18,8 +20,8 @@ createTunnel() {
     fi
 }
 ## Run the 'ls' command remotely.  If it returns non-zero, then create a new connection
-/usr/bin/ssh -p 19922 childcount@localhost ls
+/usr/bin/ssh -p $3 root@localhost ls
 if [[ $? -ne 0 ]]; then
     echo Creating new tunnel connection using key $1 "for port" $2
-    createTunnel $1 $2
+    createTunnel $1 $2 $3
 fi
